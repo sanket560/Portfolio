e2c61e3fa352f0e18e15db1b5521d7c2b60c1987
sanket mane <sanketmmane@gmail.com>
2023-07-02 00:55:45 +0530

    added skills

--- src/Components/SkillCard.jsx
+++ src/Components/SkillCard.jsx
@@ -4,11 +4,11 @@ function SkillCard({icon,name}) {
   return (
       <div className='md:w-[120px] md:h-[120px] w-2/5 h-[120px] bg-slate-800 flex flex-col items-center justify-center rounded-xl hover:scale-105'>
         <img
-          className='w-fit'
+          className='w-14'
           src={icon}
           alt='skills-icons'
         />
-        <p className='text-white font-semibold'>{name}</p>
+        <p className='text-white font-semibold mt-2'>{name}</p>
       </div>
   );
 }
