02095574471a9c2d31d12b0dbe5e275dc478af0b
sanket mane <sanketmmane@gmail.com>
2023-07-04 01:04:21 +0530

    added skills

--- src/Components/SkillCard.jsx
+++ src/Components/SkillCard.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function SkillCard({icon,name}) {
   return (
-      <div className='md:w-[120px] md:h-[120px] w-2/5 h-[120px] bg-slate-800 flex flex-col items-center justify-center rounded-xl hover:scale-110 duration-500'>
+      <div className='md:w-[140px] md:h-[120px] w-2/5 h-[120px] bg-slate-800 flex flex-col items-center justify-center rounded-xl hover:scale-110 duration-500'>
         <img
           className='w-14'
           src={icon}
-          alt='skills-icons'
+          alt={name}
         />
         <p className='text-white font-semibold mt-2'>{name}</p>
       </div>
