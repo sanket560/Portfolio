0d79ba563a734a1f057f7c9baebaffb1b21007b3
sanket mane <sanketmmane@gmail.com>
2024-03-16 11:36:19 +0530

    updated id scrolling

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,61 +1,71 @@
+"use client"
 import Image from "next/image";
-import React from "react";
+import React,{ Component } from "react";
 
-const Header = () => {
-  return (
-    <div className='navbar fixed bg-[#0f172a] bg-opacity-40 dark:bg-grey-900 dark:bg-opacity-40 backdrop-filter backdrop-blur-lg w-full h-20 border-b text-white flex items-center justify-around gap-40 md:gap-72 z-10'>
-      <div className='logo flex items-center'>
-        <Image
-          src='/Images/logo.png'
-          className='w-full h-14 ml-4'
-          alt='logo'
-          width={100}
-          height={50}
-        />
+class Header extends Component {
+  scrollTo = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  render() {
+    return (
+      <div className='navbar fixed bg-[#0f172a] bg-opacity-40 dark:bg-grey-900 dark:bg-opacity-40 backdrop-filter backdrop-blur-lg w-full h-20 border-b text-white flex items-center justify-around gap-40 md:gap-72 z-10'>
+        <div className='logo flex items-center'>
+          <Image
+            src='/Images/logo.png'
+            className='w-full h-14 ml-4'
+            alt='logo'
+            width={100}
+            height={50}
+          />
+        </div>
+        <div className='hidden md:block'>
+        <a
+            onClick={() => this.scrollTo("home")}
+            className='mx-1 hover:bg-indigo-400 p-3 rounded-lg transition duration-700 cursor-pointer'
+          >
+            Home
+          </a>
+          <a
+            onClick={() => this.scrollTo("education")}
+            className='mx-1 hover:bg-indigo-400 p-3 rounded-lg transition duration-700 cursor-pointer'
+          >
+            Education
+          </a>
+          <a
+            onClick={() => this.scrollTo("experience")}
+            className='mx-1 hover:bg-indigo-400 p-3 rounded-lg transition duration-700 cursor-pointer'
+          >
+            Experience
+          </a>
+          <a
+            onClick={() => this.scrollTo("skills")}
+            className='mx-1 hover:bg-indigo-400 p-3 rounded-lg transition duration-700 cursor-pointer'
+          >
+            Skills
+          </a>
+          <a
+            onClick={() => this.scrollTo("project")}
+            className='mx-1 hover:bg-indigo-400 p-3 rounded-lg transition duration-700 cursor-pointer'
+          >
+            Project
+          </a>
+          <a
+            onClick={() => this.scrollTo("contact")}
+            className='mx-1 hover:bg-indigo-400 p-3 rounded-lg transition duration-700 cursor-pointer'
+          >
+            Contact
+          </a>
+        </div>
+        <div className='humburger md:hidden'>
+          <p className='text-3xl cursor-pointer'></p>
+        </div>
       </div>
-      <div className='hidden md:block'>
-        <a
-          className='mx-1 hover:bg-indigo-400 p-3 rounded-lg transition duration-700'
-          href='#home'
-        >
-          Home
-        </a>
-        <a
-          className='mx-1 hover:bg-indigo-400 p-3 rounded-lg transition duration-700'
-          href='#education'
-        >
-          Education
-        </a>
-        <a
-          className='mx-1 hover:bg-indigo-400 p-3 rounded-lg transition duration-700'
-          href='#experience'
-        >
-          Experience
-        </a>
-        <a
-          className='mx-1 hover:bg-indigo-400 p-3 rounded-lg transition duration-700'
-          href='#skills'
-        >
-          Skills
-        </a>
-        <a
-          className='mx-1 hover:bg-indigo-400 p-3 rounded-lg transition duration-700'
-          href='#project'
-        >
-          Project
-        </a>
-        <a
-          className='mx-1 hover:bg-indigo-400 p-3 rounded-lg transition duration-700'
-          href='#contact'
-        >
-          Contact
-        </a>
-      </div>
-      <div className='humburger md:hidden'>
-        <p className='text-3xl cursor-pointer'></p>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Header;
