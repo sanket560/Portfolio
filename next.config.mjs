7f161b7650c5955a37f0a92576512b778d257646
sanket mane <sanketmmane@gmail.com>
2024-03-17 10:56:37 +0530

    updated next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'img.icons8.com',
-        port: '', 
-        pathname: '/',  
-      },
-      {
-        protocol: 'https',
-        hostname: 'upload.wikimedia.org',
-        port: '',
-        pathname: '/',
-      },
-    ],
+    domains: ['img.icons8.com','upload.wikimedia.org'],
   },
 };
 
